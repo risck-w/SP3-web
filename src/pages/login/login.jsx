415479e01dd35f1de188df9ada2c7b1d9c7fda67
wangjunhao <wangjh@qutke.com>
2020-06-18 15:25:57 +0800

    增加登录界面

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import './login.less'
 import logo from './images/logo.png'
-import { Form, Input, Button} from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Form, Icon, Input, Button} from 'antd';
 
 
 /**
@@ -13,10 +12,36 @@ import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
     handleSubmit = (event) => {
 
+        // 组织事件的默认行为
+        event.preventDefault();
+
+        // 获取表单项输入数据
+        this.props.form.validateFields((err, values) => {
+            if(!err) {
+                console.log('进行ajax请求: ', values)
+            }
+        });
+    }
+    // 对密码进行自定义验证
+    validatorPwd = (rule, value, callback) => {
+        console.log(rule, value)
+        if (!value) {
+            callback('密码不能为空!')
+        } else if (value.length < 8) {
+            callback('密码长度不能小于8位')
+        } else if (value.length > 16) {
+            callback('密码长度不能大于16位')
+        } else if (!/^[a-zA-Z0-9_]+$/.test(value)) {
+            callback('用户名必须是英文、数字或下划线组成')
+        }
+        callback()
     }
 
 
-     render(){
+    render(){
+
+        const { getFieldDecorator } = this.props.form;
+
         return (
             <div className="login">
                 <header className="login-header">
@@ -25,20 +50,37 @@ import { UserOutlined, LockOutlined } from '@ant-design/icons';
                 </header>
                 <section className="login-content">
                     <h2>用户登录</h2>
-                    <Form className="login-form" onSubmit={this.handleSubmit}>
-                        <Form.Item
-                            name='username'
-                            rules={[{required: true, message: 'Place input your Username!'}]}
-                        >
-                            <Input prefix={<UserOutlined className="site-form-item-icon" style={{color: 'rgba(0,0,0,.25)'}}/>} placeholder="用户名" />
-                        </Form.Item>
-                        <Form.Item
-                            name='password'
-                            rules={[{required: true, message:'Place input your Password!'}]}
-                        >
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                        {getFieldDecorator('username', {
+                            // 声明式验证，直接使用别人定义好的验证规则进行验证
+                            'rules': [
+                                {required: true, whitespace: true, message: '用户名必须输入'},
+                                {min: 4, message: '用户名至少4位'},
+                                {max: 12, message: '用户名最多12位'},
+                                {pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数字或下划线组成'}
+                            ]
+                        })(
                             <Input
-                            prefix={<LockOutlined className="site-form-item-icon" style={{color: 'rgba(0,0,0,.25)'}}/>} type="password" placeholder="密码"
+                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                placeholder="用户名"
                             />
+                        )}
+                        </Form.Item>
+                        <Form.Item>
+                            { getFieldDecorator('password', {
+                                'rules': [
+                                    {
+                                        validator: this.validatorPwd
+                                    }
+                                ]
+                            })(
+                                <Input
+                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    type="password"
+                                    placeholder="密码"
+                                />
+                            ) }
                         </Form.Item>
 
                         <Form.Item>
@@ -53,7 +95,30 @@ import { UserOutlined, LockOutlined } from '@ant-design/icons';
      }
  }
 
-const WrapLogin = Login
+/*
+1. 高阶函数
+    1）一类特别的函数
+        a. 接受函数类型的参数
+        b. 返回值是函数
+    2）常见
+        a. 定时器： setTimeout()/setInterval()
+        b. Promise: Promise(() => {}) then(value => {}, reason => {})
+        c. 数组遍历相关的方法： foreach()/filter()/map()/reduce()/find()/findIndex()
+        d. 函数对象的bind()方法
+        e. form.create()() / getFieldDecorator()()
+    3) 高阶函数更新动态，更加具有扩展性
+
+2. 高阶组件
+ 1). 本质是一个函数
+ 2). 接受一个组件（被包装组件）, 返回一个新的组件（包装组件）, 包装组件会向被包装组件传入特定属性
+*/
+
+
+/**
+ * 包装Form组件生成一个新的组件：Form(Login)
+ * 新组件会向Form组件传递一个强大的对象属性：form
+ */
+const WrapLogin = Form.create()(Login)
 export default WrapLogin
  /**
   * 1、前台表单验证
